3621314821fedbcd4a5f854f4f87e0775b0f0750
JoaoGabriel-hub <jgabrielomm@gmail.com>
2023-09-25 20:42:36 -0300

    nliaqueestaocompleta

--- aula_7.py
+++ aula_7.py
@@ -1,6 +1,6 @@
 horas = int(input('Digite o número de horas extras que você tem: '))
 horasfalt = int(input('Digite o número de horas que você faltou: '))
-if horas > horasfalt:
+if horas > (horasfalt * 1.5):
     print ('Você ganhará 500 reais a mais')
 else:
     print('Vai trabalhar mais vagabundo.')
