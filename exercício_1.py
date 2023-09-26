09a0f34ef0cdec57c3318a3baf39e21703b5407d
JoaoGabriel-hub <jgabrielomm@gmail.com>
2023-09-25 21:19:31 -0300

    soburro

--- exercício_1.py
+++ exercício_1.py
@@ -1,7 +1,10 @@
 compra = float(input(' Digite o valor total da sua compra: '))
 if compra > 100:
-    print('Você recebeu um desconto de 10%!')
+    compra -= (compra * 0.1)
+    print(f' Você recebeu um desconto de 10%, a sua compra saiu em um total de {compra}!')
 elif compra < 101 and compra > 49:
-    print(' Você recebeu um desconto de 5%! ')
-else:
-    print('Você não receberá nenhum desconto :(')
+    compra -= (compra * 0.05)
+    print(f' Você recebeu um desconto de 5%, a sua compra saiu em um total de {compra}!')
+elif compra < 50: 
+    compra = compra
+    print(f'Você não receberá nenhum desconto, a sua compra saiu em um total de {compra} :(')
