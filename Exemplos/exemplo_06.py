75e12c009e5d48b9ec506acaa784dbd3ed6f7034
JoaoGabriel-hub <jgabrielomm@gmail.com>
2023-10-09 21:30:11 -0300

    Aula 08

---
+++ Exemplos/exemplo_06.py
@@ -0,0 +1,8 @@
+# Tupla 
+
+sites_legais = (
+    'http://www.uol.com.br',
+    'http://www.g1.globo.com'
+    
+)
+
---
+++ Exemplos/exemplo_07.py
@@ -0,0 +1,14 @@
+#Dicionário
+
+filmes = {
+    1: ['Outono em NY', 'Romance', 789, 120],
+    2: ['Exorcista', 'Terror', 456, 130], 
+    3: ['Ilha do Medo', 'Suspense', 123, 120]
+
+}
+
+for filme in filmes:
+    print('f Filme>: {filmes[indice][0]}')
+    print('f Gênero>: {filmes[indice][1]}')
+    print('f Duração>: {filmes[indice][3]}')
+    print('f -----------------')            
