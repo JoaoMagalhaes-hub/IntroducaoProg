a15dfd23cae0449db0cb15f6cf14d30e24d614ac
JoaoGabriel-hub <jgabrielomm@gmail.com>
2023-10-09 21:07:11 -0300

    Conteúdo da aula 08

--- Aulas/aula_8.py
+++ Aulas/aula_8.py
@@ -8,3 +8,10 @@ print(lista_de_series[0])
 
 for serie in lista_de_series:
     print(f' A melhor série é {serie.lower()}')
+
+#Identificador nome e uma lista de nomes 
+# - nomes = ['Messias', 'Emanuel', 'Miguel', 'João']
+# Imprimir a lista
+# - print(nomes)
+#Acessando um elemento da lista 
+# - nome[0]
